db2edce6cc124ab7cc851270876b922a960a962c
AnnKrn <anakrn.carreon@gmail.com>
2017-10-24 15:24:13 -0500

    subiendo correciones

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var comparacion = funcion (array1,array2){
+var comparacion = function (array1,array2){
   if (array1.length !== array2.length) {
     return false;
   } else {
@@ -14,4 +14,4 @@ var comparacion = funcion (array1,array2){
 var arreglo1 =[1,2,3,4,5,6,7,8,9,0]
 var arreglo2 =[1,2,6,4,5,6,7,8,9,0]
 
-comparacion (arreglo1,arreglo2)
+console.log(comparacion (arreglo1,arreglo2))
